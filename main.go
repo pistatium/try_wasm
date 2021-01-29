172d4c5dba7261bd7e2ac3872b5e045d84374356
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-01-29 18:24:00 +0900

    WIP

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "runtime"
+    "syscall/js"
+    "time"
+)
+
+func rewrite() {
+    t := time.Now()
+    document := js.Global().Get("document")
+    element := document.Call("getElementById", "text")
+    element.Set("innerText", t.String())
+}
 
 func main() {
-    fmt.Println("Hello, WebAssembly!")
+    fmt.Println(runtime.GOMAXPROCS(runtime.NumCPU()))
+    for range time.Tick(1 * time.Millisecond) {
+        go rewrite()
+    }
 }
